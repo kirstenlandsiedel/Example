4bbfce8d2e10f5bbfe360806239ea467c264c0c1
Kirsten Landsiedel <kirstenlandsiedel@gmail.com>
2025-02-06 10:39:17 -0800

    rm buggy test

--- src/Example.jl
+++ src/Example.jl
@@ -12,12 +12,6 @@ function type_multiply(a::Float64, b::Float64)
     a * b
 end
 
-
-function buggy_divide(a, b)
-    return a / b  # This function will fail if b == 0 (division by zero)
-end
-
-
 export greet, simple_add, type_multiply
 
 end
